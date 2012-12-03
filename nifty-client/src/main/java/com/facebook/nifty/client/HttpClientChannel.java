5397a549fcec5c5bad77f636b07fece8b2305f6e
Andrew Cox <andrewcox@fb.com>
2012-12-03 10:57:00 -0800

    Add missing license headers

--- nifty-client/src/main/java/com/facebook/nifty/client/HttpClientChannel.java
+++ nifty-client/src/main/java/com/facebook/nifty/client/HttpClientChannel.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2012 Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.facebook.nifty.client;
 
 import com.google.common.net.HttpHeaders;
@@ -107,13 +122,11 @@ public class HttpClientChannel extends AbstractClientChannel {
     public static class Factory implements NiftyClientChannel.Factory<HttpClientChannel> {
         private final String hostName;
         private final String endpointUri;
-        private final int maxMessageSize;
 
-        public Factory(String hostName, String endpointUri, int maxMessageSize)
+        public Factory(String hostName, String endpointUri)
         {
             this.hostName = hostName;
             this.endpointUri = endpointUri;
-            this.maxMessageSize = maxMessageSize;
         }
 
         @Override
@@ -134,7 +147,7 @@ public class HttpClientChannel extends AbstractClientChannel {
                 ChannelPipeline cp = Channels.pipeline();
                 cp.addLast("frameEncoder", new HttpRequestEncoder());
                 cp.addLast("frameDecoder", new HttpResponseDecoder());
-                cp.addLast("chunkAggregator", new HttpChunkAggregator(maxMessageSize));
+                cp.addLast("chunkAggregator", new HttpChunkAggregator(maxFrameSize));
                 return cp;
                 }
             };
