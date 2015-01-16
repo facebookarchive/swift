9e82476f0cd19bbdcd40f9ef5e31349d2332e029
Stuart Reynolds <git@stuartreynolds.net>
2015-01-15 20:48:32 -0800

    Remove unused.

--- swift-codec/src/main/java/com/facebook/swift/codec/ThriftCodecManager.java
+++ swift-codec/src/main/java/com/facebook/swift/codec/ThriftCodecManager.java
@@ -44,7 +44,6 @@ import com.google.common.base.Throwables;
 import com.google.common.cache.CacheBuilder;
 import com.google.common.cache.CacheLoader;
 import com.google.common.cache.LoadingCache;
-import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.reflect.TypeToken;
 import com.google.inject.Inject;
