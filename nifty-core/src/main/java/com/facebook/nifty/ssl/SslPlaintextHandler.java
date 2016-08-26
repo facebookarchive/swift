4698e97765eb26251281e87ab94cba1103214849
Subodh Iyengar <subodh@fb.com>
2016-08-25 17:40:58 -0700

    delay creating sslhadler until needed

--- nifty-core/src/main/java/com/facebook/nifty/ssl/SslPlaintextHandler.java
+++ nifty-core/src/main/java/com/facebook/nifty/ssl/SslPlaintextHandler.java
@@ -22,11 +22,11 @@ import org.jboss.netty.handler.ssl.SslHandler;
 
 public class SslPlaintextHandler extends FrameDecoder {
 
-    private final SslHandler sslHandler;
+    private final SslServerConfiguration sslServerConfiguration;
     private final String sslHandlerName;
 
-    public SslPlaintextHandler(SslHandler sslHandler, String sslHandlerName) {
-        this.sslHandler = sslHandler;
+    public SslPlaintextHandler(SslServerConfiguration sslServerConfiguration, String sslHandlerName) {
+        this.sslServerConfiguration = sslServerConfiguration;
         this.sslHandlerName = sslHandlerName;
     }
 
@@ -42,11 +42,9 @@ public class SslPlaintextHandler extends FrameDecoder {
         }
 
         if (looksLikeTLS(buffer)) {
+            SslHandler sslHandler = sslServerConfiguration.createHandler();
+            sslHandler.setIssueHandshake(true);
             ctx.getPipeline().addAfter(ctx.getName(), sslHandlerName, sslHandler);
-        } else {
-            // If the SSL handler is not used, close the ssl engine. This will clean up any native structures
-            // that the ssl engine holds on to.
-            sslHandler.getEngine().closeOutbound();
         }
 
         ctx.getPipeline().remove(this);
